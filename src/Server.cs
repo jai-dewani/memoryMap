b7e9eb810e1f8e0ca5483df3dbbb4e82d47cb7df
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2024-03-21 23:55:22 +0530

    Turning debug on

--- src/Server.cs
+++ src/Server.cs
@@ -9,8 +9,8 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-TcpClient handler = await server.AcceptTcpClientAsync(); // wait for client
-NetworkStream stream = handler.GetStream();
+using TcpClient handler = await server.AcceptTcpClientAsync(); // wait for client
+await using NetworkStream stream = handler.GetStream();
 var message = $"+PONG\r\n";
 var messageBytes = Encoding.UTF8.GetBytes(message);
 await stream.WriteAsync(messageBytes);
