b0c904b0ea9b7157d109bf694c5015efa7c83e40
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2024-03-21 23:47:40 +0530

    Add TCP client handling and send PONG message

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,7 @@
+using System.Collections.Immutable;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,4 +9,10 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+TcpClient handler = server.AcceptTcpClient(); // wait for client
+NetworkStream stream = handler.GetStream();
+var message = $"+PONG\r\n";
+var messageBytes = Encoding.UTF8.GetBytes(message);
+stream.Write(messageBytes);
+
+server.Stop();
