ac2e975d171f54e0368639e248f9317286967532
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2024-03-23 20:08:00 +0530

    Implement the ECHO command

--- src/Server.cs
+++ src/Server.cs
@@ -1,14 +1,9 @@
-using System.Collections.Immutable;
 using System.Net;
 using System.Net.Sockets;
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
-
-
-
 
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
@@ -33,17 +28,24 @@ finally
     server.Stop();
 }
 
-
 void Respond(Socket? socket)
 {
+    string[] message = new string[] { $"+PONG\r\n", $"3\r\nhey\r\n" };
     while (socket.Connected)
     {
-        byte[] data = new byte[256];
+        byte[] data = new byte[4000];
         socket.Receive(data);
         string receivedMessage = Encoding.UTF8.GetString(data);
         Console.WriteLine($"data - {receivedMessage}");
-        var message = $"+PONG\r\n";
-        var messageBytes = Encoding.UTF8.GetBytes(message);
-        socket.Send(messageBytes);
+        if (receivedMessage.Contains("echo"))
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message[1]);
+            socket.Send(messageBytes);
+        }
+        else
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message[0]);
+            socket.Send(messageBytes);
+        }
     }
 }
