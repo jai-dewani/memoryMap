7a40d6688043e26ff8aa6df223b7087ed63e9e1e
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2024-03-23 15:59:54 +0530

    Try 4

--- src/Server.cs
+++ src/Server.cs
@@ -9,14 +9,13 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-using TcpClient handler = await server.AcceptTcpClientAsync(); // wait for client
-await using NetworkStream stream = handler.GetStream();
 while (true)
 {
+    using TcpClient handler = await server.AcceptTcpClientAsync(); // wait for client
+    await using NetworkStream stream = handler.GetStream();
     var message = $"+PONG\r\n";
     var messageBytes = Encoding.UTF8.GetBytes(message);
     await stream.WriteAsync(messageBytes);
 
 }
-
 server.Stop();
