6e156c354eefd05e0667f39710ede60945a4680d
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2024-03-23 19:29:10 +0530

    Handle concurrent clients Try 1

--- src/Server.cs
+++ src/Server.cs
@@ -13,17 +13,20 @@ try
 
     server.Start();
     Console.WriteLine($"{server.LocalEndpoint.ToString()}");
-    using var handler = await server.AcceptSocketAsync(); // wait for client
     while (true)
     {
-        byte[] data = new byte[256];
-        handler.Receive(data);
-        string receivedMessage = Encoding.UTF8.GetString(data);
-        Console.WriteLine($"data - {receivedMessage}");
-        var message = $"+PONG\r\n";
-        var messageBytes = Encoding.UTF8.GetBytes(message);
-        handler.Send(messageBytes);
+        using var handler = await server.AcceptSocketAsync(); // wait for client
+        while (true)
+        {
+            byte[] data = new byte[256];
+            handler.Receive(data);
+            string receivedMessage = Encoding.UTF8.GetString(data);
+            Console.WriteLine($"data - {receivedMessage}");
+            var message = $"+PONG\r\n";
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            handler.Send(messageBytes);
 
+        }
     }
 }
 catch (Exception ex)
