f360c3e1a7160586bb522cfd01f7cb519f712b9e
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2024-03-23 19:32:20 +0530

    Handle concurrent clients Try 2

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,14 @@
 using System.Collections.Immutable;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
+
+
+
 
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
@@ -16,17 +20,7 @@ try
     while (true)
     {
         using var handler = await server.AcceptSocketAsync(); // wait for client
-        while (true)
-        {
-            byte[] data = new byte[256];
-            handler.Receive(data);
-            string receivedMessage = Encoding.UTF8.GetString(data);
-            Console.WriteLine($"data - {receivedMessage}");
-            var message = $"+PONG\r\n";
-            var messageBytes = Encoding.UTF8.GetBytes(message);
-            handler.Send(messageBytes);
-
-        }
+        Task.Run(() => Respond(handler));
     }
 }
 catch (Exception ex)
@@ -39,3 +33,13 @@ finally
 }
 
 
+void Respond(Socket? handler)
+{
+    byte[] data = new byte[256];
+    handler.Receive(data);
+    string receivedMessage = Encoding.UTF8.GetString(data);
+    Console.WriteLine($"data - {receivedMessage}");
+    var message = $"+PONG\r\n";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    handler.Send(messageBytes);
+}
