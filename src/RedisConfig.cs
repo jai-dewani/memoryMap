528a3df79125dd5a5482632c802d57e1c8931c28
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2024-03-27 01:37:05 +0530

    The INFO command try 6

--- src/RedisConfig.cs
+++ src/RedisConfig.cs
@@ -5,7 +5,6 @@ class RedisConfig
     public static List<string> GetConfig()
     {
         var response = new List<string>();
-        response.Append($"# Replication");
         response.Append($"role:{Role}");
         return response;
     }
