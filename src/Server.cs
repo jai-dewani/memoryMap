364674552b0e323ed0acd4d3e7ff9bf616cac7d2
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2024-03-27 20:10:46 +0530

    The INFO command on a replica try 5"

--- src/Server.cs
+++ src/Server.cs
@@ -7,7 +7,7 @@ foreach (var arg in argParser.args.Keys)
     switch (arg)
     {
         case "port":
-            Console.WriteLine($"Port - {argParser.args[arg]}");
+            Console.WriteLine($"Port - {string.Join(" ", argParser.args[arg])}");
             RedisConfig.Port = int.Parse(argParser.args[arg][1]);
             break;
 
